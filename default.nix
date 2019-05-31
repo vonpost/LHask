16258941fbdf6f821bc429540f49e2d18c2d9d47
vonpost <dcol@kth.se>
2019-05-31 04:09:55 +0200

    added default.nix

--- default.nix
+++ default.nix
@@ -1,4 +1,9 @@
-{ mkDerivation, base, stdenv }:
+{ mkDerivation, accelerate, accelerate-blas
+, accelerate-io-bytestring, accelerate-llvm, accelerate-llvm-native
+, accelerate-llvm-ptx, array, backprop, base, binary, deepseq
+, directory, lens, lens-accelerate, mnist-idx
+, mwc-random-accelerate, random, stdenv, transformers, vector
+}:
 mkDerivation {
   pname = "HLearn";
   version = "0.1.0.0";
@@ -6,7 +11,12 @@ mkDerivation {
   isLibrary = true;
   isExecutable = true;
   libraryHaskellDepends = [ base ];
-  executableHaskellDepends = [ base ];
+  executableHaskellDepends = [
+    accelerate accelerate-blas accelerate-io-bytestring accelerate-llvm
+    accelerate-llvm-native accelerate-llvm-ptx array backprop base
+    binary deepseq directory lens lens-accelerate mnist-idx
+    mwc-random-accelerate random transformers vector
+  ];
   doHaddock = false;
   license = stdenv.lib.licenses.bsd3;
 }
